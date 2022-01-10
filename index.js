4f73fbf4d57e68006b8ffcc13f7a63b0f2f0aeb4
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-10 11:38:38 +0000

    add error handling and update response object

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Server Dependencies
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const { fetchRawData } = require('./helpers/data-fetcher');
-const { dataConverter } = require('./helpers/converter');
-const { makePrediction } = require('./helpers/predictor');
+const { fetchRawData } = require("./helpers/data-fetcher");
+const { dataConverter } = require("./helpers/converter");
+const { makePrediction, predictionTrend } = require("./helpers/predictor");
 
 // Init Express
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 // Server Config
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,63 +18,71 @@ app.use(bodyParser.json());
 
 // Cors Controls
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'POST, GET, PATCH, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "POST, GET, PATCH, DELETE, OPTIONS"
   );
   next();
 });
 app.use(cors());
 
 // Routes Definitions
-app.get('/', (req, res) => {
-  res.send('GCP App Engine!');
+app.get("/", (req, res) => {
+  res.send("GCP App Engine!");
 });
 app.get(`/api/v1/predict/:stockSymbol`, async (req, res) => {
   const startTime = new Date().getTime();
   const stockSymbol = req.params.stockSymbol;
-  const rawData = await fetchRawData(stockSymbol);
-  const convertedData = await dataConverter(rawData);
-  const prediction = await makePrediction(convertedData);
-  try {
-    if (prediction) {
-      const endTime = new Date().getTime();
-      Math.floor((endTime - startTime) / (1000 * 60 * 60 * 24));
-      res.status(200).json({
-        message: 'Prediction made successfully!',
-        predictionData: prediction[0],
-        predictionTimeTaken: (endTime - startTime) / 1000,
-        predictionMadeOnDate: new Date().toISOString(),
-      });
-    } else {
+  const rawStockData = await fetchRawData(stockSymbol.trim());
+  if (!rawStockData.success) {
+    res.status(400).json({
+      message: "Stock symbol not found",
+    });
+  } else {
+    const convertedData = await dataConverter(rawStockData.data);
+    const prediction = await makePrediction(convertedData);
+    try {
+      if (prediction) {
+        const trend = await predictionTrend(prediction[0]);
+        const endTime = new Date().getTime();
+        res.status(200).json({
+          message: "Prediction made successfully!",
+          stockSymbol: stockSymbol,
+          predictionData: prediction[0],
+          priceTrend: trend,
+          predictionTimeTaken: (endTime - startTime) / 1000,
+          predictionMadeOnDate: new Date().toISOString(),
+        });
+      } else {
+        res.status(500).json({
+          message: "Prediction failed",
+        });
+      }
+    } catch (error) {
       res.status(500).json({
-        message: 'Prediction failed',
+        message: "General error",
       });
     }
-  } catch (error) {
-    res.status(500).json({
-      message: 'General error',
-    });
   }
 });
 app.get(`/api/v1/test`, async (req, res) => {
   res.status(200).json({
-    message: 'Success',
+    message: "Success",
   });
 });
 
 // 404 Handling
 app.use((req, res) => {
-  res.status(404).send({ url: req.originalUrl + ' not found' });
+  res.status(404).send({ url: req.originalUrl + " not found" });
 });
 
 // Server Port Controls
-const port = process.env.PORT || '8080';
-app.set('port', port);
+const port = process.env.PORT || "8080";
+app.set("port", port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
